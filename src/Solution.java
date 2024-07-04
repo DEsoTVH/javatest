3ecfce82bbe6cee98b7ac8ce3ec2c2e89278b8ee
desotvh <berendicto@gmail.com>
2024-07-03 23:06:33 -0400

    practicando bucles

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
-
 public class Solution {
-    public static String secret = "AmIGo";
-
     public static void main(String[] args) {
-
+        String text = " loves me.";
         Scanner scan = new Scanner(System.in);
-        String password = scan.nextLine();
 
-        if (password.equalsIgnoreCase(secret)) {
-            System.out.println("Acceso permitido");
-        } else {
-            System.out.println("Acceso denegado");
+        String name = scan.nextLine();
+        int n = 0;
 
+        while (n < 10) {
+            System.out.println(name + text);
+            n++;
         }
-    }
 
+
+    }
 }
